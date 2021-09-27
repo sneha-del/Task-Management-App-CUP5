835c0d0d64377c461b9b4cfd5f614b30f1ab9504
sneha <72269836+sneha-del@users.noreply.github.com>
2021-09-27 13:58:47 +0530

    Added User data page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Details = ({ user }) => {
+  console.log(user);
   return (
     <>
       <div className="details-page">
-        <h1>Hey this is Details page of TODO</h1>
+        <h1>Hey this is Details page of USER</h1>
+
+        <ul>
+          <li className="li"> Your Email: {user.email}</li>
+        </ul>
       </div>
     </>
   );
